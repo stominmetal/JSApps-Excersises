17ce27751f348d1fb8e4bc45ee72ffc10c83862f
stominmetal <stoyan@astrapaging.com>
2016-11-27 19:46:16 +0200

    Added Register functionality

--- SpaAjaxKinvey/scripts/books-library.js
+++ SpaAjaxKinvey/scripts/books-library.js
@@ -1,3 +1,158 @@
 function startApp() {
+    sessionStorage.clear(); // Clear user auth data
 
+    const kinveyBaseUrl = "https://baas.kinvey.com/";
+    const kinveyAppKey = "kid_ryhQZs9B";
+    const kinveyAppSecret = "ca5c5d5247004c9ba9dc9b6158978d13";
+    const kinveyAppAuthHeaders = {
+        'Authorization': "Basic " + btoa(kinveyAppKey + ":" + kinveyAppSecret),
+    };
+
+    showHideMenuLinks();
+
+    showView('viewHome');
+
+    // Bind the navigation menu links
+    $("#linkHome").click(showHomeView);
+
+    // Bind the form submit buttons
+    $("#buttonLoginUser").click(loginUser);
+    $("#buttonRegisterUser").click(registerUser);
+    $("#buttonCreateBook").click(createBook);
+    $("#buttonEditBook").click(editBook);
+
+    // Bind the navigation menu links
+    $("#linkLogin").click(showLoginView);
+    $("#linkRegister").click(showRegisterView);
+    $("#linkListBooks").click(listBooks);
+    $("#linkCreateBook").click(showCreateBookView);
+    $("#linkLogout").click(logoutUser);
+
+    // Bind the info / error boxes: hide on click
+    $("#infoBox, #errorBox").click(function () {
+        $(this).fadeOut();
+    });
+
+    // Attach AJAX "loading" event listener
+    $(document).on({
+        ajaxStart: function () {
+            $("#loadingBox").show()
+        },
+        ajaxStop: function () {
+            $("#loadingBox").hide()
+        }
+    });
+
+    function showHideMenuLinks() {
+        $("#menu a").hide();
+        if (sessionStorage.getItem("authToken")) {
+            $("#linkHome").show();
+            $("#linkListBooks").show();
+            $("#linkCreateBook").show();
+            $("#linkLogout").show();
+        } else {
+            $("#linkHome").show();
+            $("#linkLogin").show();
+            $("#linkRegister").show();
+        }
+    }
+
+    function showView(viewName) {
+        // Hide all views and show the selected view only
+        $('main > section').hide();
+        $('#' + viewName).show();
+    }
+
+    function showHomeView() {
+        showView("viewHome");
+    }
+
+    function showRegisterView() {
+        $('#formRegister').trigger('reset');
+        showView('viewRegister');
+    }
+
+    function listBooks() {
+        //
+    }
+
+    function showCreateBookView() {
+        $('#formCreateBook').trigger('reset');
+        showView('viewCreateBook');
+    }
+
+    function showLoginView() {
+        showView('viewLogin');
+        $('#formLogin').trigger('reset');
+        // sessionStorage.setItem("authToken", 'asdad')
+        // showHideMenuLinks();
+    }
+
+    function logoutUser() {
+
+    }
+
+    function loginUser() {
+
+    }
+
+    function registerUser() {
+        let userData = {
+            username: $('#formRegister input[name=username]').val(),
+            password: $('#formRegister input[name=passwd]').val()
+        };
+        $.ajax({
+            method: "POST",
+            url: kinveyBaseUrl + "user/" + kinveyAppKey + "/",
+            headers: kinveyAppAuthHeaders,
+            data: userData,
+            success: registerSuccess,
+            error: handleAjaxError
+        })
+
+        function registerSuccess(userInfo) {
+            saveAuthInSession(userInfo);
+            showHideMenuLinks();
+            listBooks();
+            showInfo('User registration successful.');
+        }
+    }
+
+    function saveAuthInSession(userInfo) {
+        let userAuth = userInfo._kmd.authtoken;
+        sessionStorage.setItem('authToken', userAuth);
+        let userId = userInfo._id;
+        sessionStorage.setItem('userId', userId);
+        let username = userInfo.username;
+        $('#loggedInUser').text("Welcome, " + username + "!");
+    }
+
+    function showInfo(message) {
+        $('#infoBox').text(message);
+        $('#infoBox').show();
+        setTimeout(function() {
+            $('#infoBox').fadeOut();
+        }, 3000);
+    }
+
+    function handleAjaxError(response) {
+        let errorMsg = JSON.stringify(response);
+        if (response.readyState === 0)
+            errorMsg = "Cannot connect due to network error.";
+        if (response.responseJSON && response.responseJSON.description) errorMsg = response.responseJSON.description;
+        showError(errorMsg);
+    }
+
+    function showError(errorMsg) {
+        $('#errorBox').text("Error: " + errorMsg);
+        $('#errorBox').show();
+    }
+
+    function createBook() {
+
+    }
+
+    function editBook() {
+
+    }
 }
